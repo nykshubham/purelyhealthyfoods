7b6caaec169722500e6ce687aa7d7438ccf81756
Shubham Nyk <shubhamnyk@Shubhams-MacBook-Air.local>
2026-01-04 15:37:41 +0530

    No content

--- netlify/functions/scheduled-content.ts
+++ netlify/functions/scheduled-content.ts
@@ -1,8 +1,7 @@
+// import { schedule } from '@netlify/functions';
+// import { main } from './generateContent';
 
-import { schedule } from '@netlify/functions';
-import { main } from './generateContent';
-
-export const handler = schedule('0 * * * *', async () => {
-    await main();
-    return { statusCode: 200 };
-});
+// export const handler = schedule('0 * * * *', async () => {
+//     await main();
+//     return { statusCode: 200 };
+// });
